2e260a15c74d568b0ee0b0208257d5f15de3fb76
KennethLG <orsonleiziaga@gmail.com>
2024-07-10 19:40:29 -0500

    adding proto files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const protoPath = join(__dirname, 'hero/hero.proto');
+  console.log('🚀 ~ bootstrap ~ protoPath:', protoPath);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'hero',
+      protoPath,
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
